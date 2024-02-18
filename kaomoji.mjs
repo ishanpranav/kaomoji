4ccb098233e88dff8e4acb8b5f8943603c9fec76
Ishan Pranav <ishan.pranav@outlook.com>
2024-02-17 22:51:44 -0500

    Implement Kaomoji class

--- kaomoji.mjs
+++ kaomoji.mjs
@@ -1,3 +1,34 @@
 // kaomoji.mjs
 // Copyright (c) 2024 Ishan Pranav
 // Licensed under the MIT license.
+
+/** Represents a Kaomoji. */
+export class Kaomoji {
+    /**
+     * Initializes a new instance of the `Kaomoji` class.
+     * 
+     * @param {String} value    a string representation of the kaomoji.
+     * @param {Array}  emotions an array of keywords describing the kaomoji.
+     */
+    constructor(value, emotions) {
+        this.value = value;
+        this.emotions = emotions;
+        this.emotionSet = new Set();
+
+        for (const emotion of emotions) {
+            this.emotionSet.add(emotion.toLowerCase());
+        }
+    }
+
+    /**
+     * Determines if a given keyword describes the kaomoji. This method uses a
+     * case insensitive, amortized constant-time algorithm.
+     * 
+     * @param {String} value the keyword to test.
+     * @return {Boolean} `true` if the given value describes the kaomoji;
+     *                   otherwise, `false`.
+     */
+    isEmotion(value) {
+        return this.emotionSet.has(value.toLowerCase());
+    }
+}
